978fd92231ed3b9e4c35a0e84e2944dddc91e13a
qili.gu <sh56053687@gmail.com>
2020-10-24 12:07:06 -0700

    Initilize the first version.

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP Wed Oct 21 20:48:03 PDT 2020"
+#define LINUX_COMPILE_BY "augustine"
+#define LINUX_COMPILE_HOST "docker"
+#define LINUX_COMPILER "gcc version 4.9.3 (crosstool-NG crosstool-ng-1.22.0-88-g8460611)"
